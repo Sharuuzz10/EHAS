e3015dd544348b4c2ab1d48ad32dc6e0d2dd1731
sharon <sharonbk8547@gmail.com>
2025-03-13 13:10:13 +0530

    your commit message

---
+++ EHAS_teacher/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="content">
+      <h1>Welcome to Student Management</h1>
+    </div>
+  );
+};
+
+export default Home;
